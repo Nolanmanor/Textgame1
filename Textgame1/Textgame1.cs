27a19d7f523dcde56ba1f6c9e93638f6ce2aa931
Nolan <nolanjmc707@gmail.com>
2020-03-05 12:07:46 -0800

    current location

--- Textgame1/Textgame1.cs
+++ Textgame1/Textgame1.cs
@@ -96,6 +96,7 @@ namespace Textgame1
         {
             if (propertyChangedEventArgs.PropertyName == "Weapons")
             {
+
                 cboWeapons.DataSource = _player.Weapons;
 
                 if (!_player.Weapons.Any())
@@ -115,6 +116,35 @@ namespace Textgame1
                     btnUsePotion.Visible = false;
                 }
             }
+
+            if (propertyChangedEventArgs.PropertyName == "CurrentLocation")
+            {
+                // Show/hide available movement buttons
+                btnNorth.Visible = (_player.CurrentLocation.LocationToNorth != null);
+                btnEast.Visible = (_player.CurrentLocation.LocationToEast != null);
+                btnSouth.Visible = (_player.CurrentLocation.LocationToSouth != null);
+                btnWest.Visible = (_player.CurrentLocation.LocationToWest != null);
+
+                // Display current location name and description
+                rtbLocation.Text = _player.CurrentLocation.Name + Environment.NewLine;
+                rtbLocation.Text += _player.CurrentLocation.Description + Environment.NewLine;
+
+                if (_player.CurrentLocation.MonsterLivingHere == null)
+                {
+                    cboWeapons.Visible = false;
+                    cboPotions.Visible = false;
+                    btnUseWeapon.Visible = false;
+                    btnUsePotion.Visible = false;
+                }
+                else
+                {
+                    cboWeapons.Visible = _player.Weapons.Any();
+                    cboPotions.Visible = _player.Potions.Any();
+                    btnUseWeapon.Visible = _player.Weapons.Any();
+                    btnUsePotion.Visible = _player.Potions.Any();
+                }
+            }
+
         }
 
         private void btnNorth_Click(object sender, EventArgs e)
